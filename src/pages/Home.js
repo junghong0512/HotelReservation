c8518084d9ce01f86072afa3083957c9306d591b
Jung <junghong91@naver.com>
2020-05-28 01:14:37 +0900

    How to use React Context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import Services from "../components/Services";
 import { Link } from "react-router-dom";
+import FeaturedRooms from "../components/FeaturedRooms";
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
         </Banner>
       </Hero>
       <Services />
+      <FeaturedRooms />
     </>
   );
 };
