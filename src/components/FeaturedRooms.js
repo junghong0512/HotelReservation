bcecc7278a2fe4faad464f25ff0296b25b2c761c
Jung <junghong91@naver.com>
2020-05-28 01:21:04 +0900

    Loading.js is done

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Loading from "./Loading";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
     const { featuredRooms: rooms } = this.context;
     console.log(rooms);
-    return <div>hello from FeaturedRoom</div>;
+    return (
+      <div>
+        hello from FeaturedRoom
+        <Loading />
+      </div>
+    );
   }
 }
