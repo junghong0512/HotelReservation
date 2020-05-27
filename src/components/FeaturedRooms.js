4982c1b008cffa526c4c1e5684db8beb4e26ffeb
Jung <junghong91@naver.com>
2020-05-28 01:40:48 +0900

    In FeaturedRooms.js use react context loading and featuredRooms data

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Title from "./Title";
 import Loading from "./Loading";
+import Room from "./Room";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { featuredRooms: rooms } = this.context;
-    console.log(rooms);
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map((room) => {
+      return <Room key={room.id} room={room} />;
+    });
     return (
-      <div>
-        hello from FeaturedRoom
-        <Loading />
-      </div>
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
     );
   }
 }
