d1cceb1eaa8ac3b124594615ef0d3b0bb5f811e0
Jung <junghong91@naver.com>
2020-05-28 16:31:55 +0900

    getRoom function on the context.js, PropType is added to the Room.js, and Banner of the SingleRoom.js is added

--- src/context.js
+++ src/context.js
@@ -35,12 +35,15 @@ export default class RoomProvider extends Component {
     return tempItems;
   }
 
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider
-        // value={{ greeting: this.state.greeting, name: this.state.name }}
-        value={{ ...this.state }}
-      >
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
