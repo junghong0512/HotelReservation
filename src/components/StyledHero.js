a4c66d0ab459f07250ff4cb3ba3487fba833743a
Jung <junghong91@naver.com>
2020-05-29 11:56:04 +0900

    Header background image changed by props is added by StyledHero.js, and singleRoom images are added

---
+++ src/components/StyledHero.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import defaultImg from "../images/room-1.jpeg";
+
+const SimpleHero = styled.header`
+  min-height: 60vh;
+  display: flex;
+  background: url(${(props) => (props.img ? props.img : defaultImg)})
+    center/cover no-repeat;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default SimpleHero;
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
+import StyledHero from "../components/StyledHero";
 
 export default class SingleRoom extends Component {
   constructor(props) {
@@ -45,13 +46,22 @@ export default class SingleRoom extends Component {
       images,
     } = room;
     return (
-      <Hero hero="roomsHero">
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            back to rooms
-          </Link>
-        </Banner>
-      </Hero>
+      <>
+        <StyledHero img={images[0] || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {images.map((image, index) => {
+              return <img key={index} src={image} alt={name} />;
+            })}
+          </div>
+        </section>
+      </>
     );
   }
 }
