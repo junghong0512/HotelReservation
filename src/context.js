30508afc8e96df193183f48fb6d6c1adc8a9e70c
Jung <junghong91@naver.com>
2020-06-02 02:48:54 +0900

    Type selecting filter of the  RoomFilter

--- src/context.js
+++ src/context.js
@@ -9,6 +9,15 @@ export default class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPirce: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   // getData
@@ -17,11 +26,17 @@ export default class RoomProvider extends Component {
     // this.getData
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter((room) => room.featured === true);
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
+
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   }
 
@@ -41,9 +56,27 @@ export default class RoomProvider extends Component {
     return room;
   };
 
+  handleChange = (event) => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
+
+    console.log(`type: ${type}, name: ${name}, value: ${value}`);
+  };
+
+  filterRooms = () => {
+    console.log("hello");
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange,
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
